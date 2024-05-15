782e056a482063bcec6dc9cd21abfd0d08820c13
Jay Becker <jay@cryptoconcierge.consulting>
2024-05-15 00:10:21 -0700

    added extends to tailwinds and created web link and guest about pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { RouterModule, Routes,  TitleStrategy, RouterStateSnapshot } from '@angu
 import { GuestProfileComponent } from './guest-profile/guest-profile.component';
 import { SitemapComponent } from './sitemap/sitemap.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { A01c2GuestAboutComponent } from './a01c2-guest-about/a01c2-guest-about.component'
+import { A01c4GuestWebLinksComponent } from './a01c4-guest-web-links/a01c4-guest-web-links.component'
 
 const routes: Routes = [
   { path: 'guest', component: GuestProfileComponent },
+  { path: 'guest-about', component: A01c2GuestAboutComponent },
+  { path: 'guest-weblinks', component: A01c4GuestWebLinksComponent },
   { path: 'sitemap', component: SitemapComponent },
   { path: 'page-not-found', component: PageNotFoundComponent },
   { path: '',  redirectTo: 'guest', pathMatch: "full" },
