d406a915b9abdcbac98be143832c7b1e65a30e0b
michael-gokey-architect <michael.gokey.architect@gmail.com>
2024-05-17 15:10:31 -0400

    basic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {bootstrapApplication} from '@angular/platform-browser';
-import {provideHttpClient} from '@angular/common/http';
-import {provideAnimations, BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// import {VERSION as CDK_VERSION} from '@angular/cdk';
-import {VERSION as MAT_VERSION, MatNativeDateModule} from '@angular/material/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
