e2ca4312803f23a5ee4bfa270c84d25c3cd809e5
Jay Becker <jay@cryptoconcierge.consulting>
2024-05-21 22:57:48 -0700

    fixed component errors on main

---
+++ src/app/guest-connection-indicator/guest-connection-indicator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-connection-indicator',
+  templateUrl: './guest-connection-indicator.component.html',
+  styleUrls: ['./guest-connection-indicator.component.css']
+})
+export class GuestConnectionIndicatorComponent {
+
+}
