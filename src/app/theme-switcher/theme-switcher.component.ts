08eef89aa60caa56c9d28142b995f06a14a57f27
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-05-28 19:32:25 -0400

    Simplify Function to Set Theme

--- src/app/theme-switcher/theme-switcher.component.ts
+++ src/app/theme-switcher/theme-switcher.component.ts
@@ -6,40 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./theme-switcher.component.css'],
 })
 export class ThemeSwitcherComponent {
-  defaultTheme = 'protoGrays';
-
-  theme = {
-    protoGrays: false,
-    pathWRQ: false,
-    nyAutoshow: false,
-    dragonCon: false,
-  };
+	defaultTheme = 'protoGrays';
+	theme=''
 
   setTheme(selectedTheme: string) {
-    if (selectedTheme === 'protoGrays') {
-      this.theme.protoGrays = true;
-      this.theme.pathWRQ = false;
-      this.theme.nyAutoshow = false;
-      this.theme.dragonCon = false;
-    }
-    if (selectedTheme === 'pathWRQ') {
-      this.theme.protoGrays = false;
-      this.theme.pathWRQ = true;
-      this.theme.nyAutoshow = false;
-      this.theme.dragonCon = false;
-    }
-    if (selectedTheme === 'nyAutoshow') {
-      this.theme.protoGrays = false;
-      this.theme.pathWRQ = false;
-      this.theme.nyAutoshow = true;
-      this.theme.dragonCon = false;
-    }
-    if (selectedTheme === 'dragonCon') {
-      this.theme.protoGrays = false;
-      this.theme.pathWRQ = false;
-      this.theme.nyAutoshow = false;
-      this.theme.dragonCon = true;
-    }
+		this.theme = selectedTheme;
   }
 }
 
