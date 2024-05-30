ebf006a27001ba8b776f4574322d2b4075c759ed
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-05-29 20:09:39 -0400

    Update Options to Dropdown and Functionality

--- src/app/theme-switcher/theme-switcher.component.ts
+++ src/app/theme-switcher/theme-switcher.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-theme-switcher',
   templateUrl: './theme-switcher.component.html',
   styleUrls: ['./theme-switcher.component.css'],
 })
-export class ThemeSwitcherComponent {
-	defaultTheme = 'protoGrays';
-	theme=''
-
-  setTheme(selectedTheme: string) {
-		this.theme = selectedTheme;
-  }
+	
+export class ThemeSwitcherComponent implements OnInit {
+	theme: string = 'protoGrays'
+	
+	ngOnInit() {
+		let switcher = document.getElementById("themeoptions")! as HTMLSelectElement;
+		switcher?.addEventListener('change', (e: Event) => {
+			let selectedThemeIndex: number = switcher.options.selectedIndex
+			this.theme = switcher.options[selectedThemeIndex].getAttribute('value') || 'protoGrays';
+		})
+	}
 }
 
 
