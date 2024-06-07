84189812baa917e943b3f2ffc768db4cb72dcb27
Jay Becker <jay@cryptoconcierge.consulting>
2024-06-07 13:16:22 -0700

    Added event shcedule page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,6 +45,7 @@ import { A01bGuestWelcomeBackComponent } from './built/a01b-guest-welcome-back/a
 import { B04aVendorAboutLandingDisplayComponent } from './built/b04a-vendor-about-landing-display/b04a-vendor-about-landing-display.component';
 import { A05cGuestRequestsLandingComponent } from './a05c-guest-requests-landing/a05c-guest-requests-landing.component';
 import { E08aEventHomeComponent } from './b-e08a-event-home/e08a-event-home.component';
+import { E08bEventScheduleComponent } from './b-e08b-event-schedule/e08b-event-schedule.component';
 
 
 
@@ -57,6 +58,7 @@ const routes: Routes = [
   { path: 'org-details', component: OrgDetailsComponent},
   { path: 'index-landing', component: IndexLandingComponent},
   { path: 'event-home', component: E08aEventHomeComponent},
+  { path: 'event-schedule', component: E08bEventScheduleComponent},
   { path: 'guest-requests', component: A05cGuestRequestsLandingComponent},
 
 
